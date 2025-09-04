450bf921013370661d6fd39fa7f3cb60ea0a13cc
Juan Velasquez <valenelnoobatoyt@gmail.com>
2025-09-04 17:14:20 -0300

    Se agrego precios a los producos y se arreglo el problema con el stock de los productos

--- lib/compras.ex
+++ lib/compras.ex
@@ -1,100 +1,76 @@
 defmodule Libremarket.Compras do
 
-  @doc """
-  Modificar el metodo para ingresar el numero del pedido compra
-  """
-  def comprar(producto) do
-    case Libremarket.Ventas.Server.comprar(producto) do
+  def comprar(producto_id) when is_integer(producto_id) do
+    case Libremarket.Ventas.Server.comprar(producto_id) do
+      {:error, :producto_invalido} ->
+        {:error, :producto_invalido}
+
       {:error, :sin_stock} ->
         {:error, :sin_stock}
 
-      {:ok, producto} ->
+      {:ok, producto_actualizado} ->
+        # Confirmación del cliente (80%)
         if confirmar_compra?() do
           id_compra = :erlang.unique_integer([:positive])
 
-          # El modulo de Pagos detecta infracciones en el pago
+          # Autorizar pago (70%)
           case Libremarket.Pagos.Server.autorizar_pago(id_compra) do
-            # El modulo detecta infraccion
             false ->
-              Libremarket.Ventas.Server.liberar(producto)
+              # Pago rechazado → liberar producto
+              Libremarket.Ventas.Server.liberar(producto_id)
               {:error, :pago_rechazado}
 
-            # El modulo no detecta infraccion
             true ->
-              # El modulo de Infracciones detecta infracciones en la compra
+              # Detectar infracciones (30%)
               case Libremarket.Infracciones.Server.detectar_infraccion(id_compra) do
-                # Se encuentra infraccion
                 true ->
-                  Libremarket.Ventas.Server.liberar(producto)
+                  Libremarket.Ventas.Server.liberar(producto_id)
                   {:error, :infraccion_detectada}
 
-                # No se encuentra infraccion
                 false ->
                   envio = elegir_envio()
                   {:ok, envio_info} =
-                    Libremarket.Envios.Server.registrar(id_compra, producto, envio)
+                    Libremarket.Envios.Server.registrar(id_compra, producto_id, envio, producto_actualizado.precio)
+
+                  total = producto_actualizado.precio + envio_info.costo_envio
 
                   {:ok,
-                   %{id: id_compra, producto: producto, envio: envio, costo: envio_info.costo}}
+                   %{
+                     id: id_compra,
+                     producto_id: producto_id,
+                     nombre: producto_actualizado.name,
+                     precio: producto_actualizado.precio,
+                     envio: envio,
+                     costo_envio: envio_info.costo_envio,
+                     total: total
+                   }}
               end
           end
-        # Se cancela la compra del producto
         else
-          Libremarket.Ventas.Server.liberar(producto)
-          {:cancelada, producto}
+          # Cliente canceló
+          Libremarket.Ventas.Server.liberar(producto_id)
+          {:cancelada, producto_id}
         end
     end
   end
 
-  defp confirmar_compra?() do
-    Enum.random(1..100) <= 80
-  end
-
-  defp elegir_envio() do
-    if Enum.random(1..100) <= 70 do
-      :correo
-    else
-      :retiro
-    end
-  end
+  defp confirmar_compra?(), do: Enum.random(1..100) <= 80
+  defp elegir_envio(), do: if(Enum.random(1..100) <= 70, do: :correo, else: :retiro)
 end
 
 defmodule Libremarket.Compras.Server do
-  @moduledoc """
-  Compras
-  """
-
   use GenServer
 
-  # API del cliente
+  def start_link(opts \\ %{}), do: GenServer.start_link(__MODULE__, opts, name: __MODULE__)
 
-  @doc """
-  Crea un nuevo servidor de Compras
-  """
-  def start_link(opts \\ %{}) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
-  end
+  def comprar(pid \\ __MODULE__, producto_id), do: GenServer.call(pid, {:comprar, producto_id})
 
-  def comprar(pid \\ __MODULE__) do
-    GenServer.call(pid, :comprar)
-  end
-
-  # Callbacks
-
-  @doc """
-  Inicializa el estado del servidor
-  """
   @impl true
-  def init(state) do
-    {:ok, state}
-  end
+  def init(_opts), do: {:ok, %{}}
 
-  @doc """
-  Callback para un call :comprar
-  """
   @impl true
-  def handle_call(:comprar, _from, state) do
-    result = Libremarket.Compras.comprar
+  def handle_call({:comprar, producto_id}, _from, state) do
+    result = Libremarket.Compras.comprar(producto_id)
     {:reply, result, state}
   end
 end
--- lib/ventas.ex
+++ lib/ventas.ex
@@ -1,63 +1,72 @@
 defmodule Libremarket.Ventas do
-  @productos ["Laptop", "Teclado", "Mouse", "Monitor", "Auricular", "Impresora", "Camara", "Tablet", "Router", "Microfono"]
-
-  use Application
+  @productos [
+    "Laptop", "Teclado", "Mouse", "Monitor", "Auricular",
+    "Impresora", "Camara", "Tablet", "Router", "Microfono"
+  ]
 
   def productos_iniciales() do
-    Enum.map(@productos, fn prod ->
-      {prod, Enum.random(1..10)}
+    Enum.into(1..length(@productos), %{}, fn id ->
+      name = Enum.at(@productos, id - 1)
+      stock = Enum.random(1..10)
+      precio = Enum.random(100..2000)
+      {id, %{name: name, stock: stock, precio: precio}}
     end)
-    |> Map.new()
   end
 end
 
 defmodule Libremarket.Ventas.Server do
   use GenServer
 
-  def start_link(opts \\ %{}) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
-  end
+  # API
+  def start_link(opts \\ %{}), do: GenServer.start_link(__MODULE__, opts, name: __MODULE__)
 
-  def listar_stock(pid \\ __MODULE__, producto) do
-    GenServer.call(pid, :listar)
-  end
+  def listar_stock(pid \\ __MODULE__), do: GenServer.call(pid, :listar)
 
-  def comprar(pid \\ __MODULE__, producto) do
-    GenServer.call(pid, {:comprar, producto})
-  end
+  def comprar(pid \\ __MODULE__, producto_id, cantidad \\ 1),
+    do: GenServer.call(pid, {:comprar, producto_id, cantidad})
 
-  def liberar(pid \\ __MODULE__, producto) do
-    GenServer.call(pid, {:liberar, producto})
-  end
+  def liberar(pid \\ __MODULE__, producto_id, cantidad \\ 1),
+    do: GenServer.call(pid, {:liberar, producto_id, cantidad})
 
+  # Callbacks
   @impl true
   def init(_opts) do
     {:ok, Libremarket.Ventas.productos_iniciales()}
   end
 
+  @impl true
   def handle_call(:listar, _from, state) do
     {:reply, state, state}
   end
 
-  def handle_call({:comprar, producto}, _from, state) do
-    case Map.get(state, producto, 0) do
-      # Si no hay productos, manda un mensaje de error
-      0 -> {:reply, {:error, :sin_stock}, state}
-      # Si hay productos, lo descuenta de la lista de productos
-      stock ->
-        new_state = Map.put(state, producto, stock - 1)
-        {:reply, {:ok, producto}, new_state}
+  @impl true
+  def handle_call({:comprar, id, cantidad}, _from, state)
+      when is_integer(id) and is_integer(cantidad) and cantidad > 0 do
+    case Map.fetch(state, id) do
+      :error ->
+        {:reply, {:error, :producto_invalido}, state}
+
+      {:ok, %{stock: stock} = prod} when stock < cantidad ->
+        {:reply, {:error, :sin_stock}, state}
+
+      {:ok, %{stock: stock} = prod} ->
+        new_prod = Map.put(prod, :stock, stock - cantidad)
+        new_state = Map.put(state, id, new_prod)
+        {:reply, {:ok, new_prod}, new_state}
     end
   end
 
-  def handle_call({:liberar, producto}, _from, state) do
-    case Map.get(state, producto) do
-      # Si no existe el producto, manda un mensaje de error
-      nil -> {:reply, {:error, :producto_invalido}, state}
-      # Si hay productos, lo descuenta de la lista de productos
-      stock ->
-        new_state = Map.put(state, producto, stock + 1)
-        {:reply, {:ok, producto}, new_state}
+  @impl true
+  def handle_call({:liberar, id, cantidad}, _from, state)
+      when is_integer(id) and is_integer(cantidad) and cantidad > 0 do
+    case Map.fetch(state, id) do
+      :error ->
+        {:reply, {:error, :producto_invalido}, state}
+
+      {:ok, %{stock: stock} = prod} ->
+        new_prod = Map.put(prod, :stock, stock + cantidad)
+        new_state = Map.put(state, id, new_prod)
+        {:reply, {:ok, new_prod}, new_state}
     end
   end
 end
