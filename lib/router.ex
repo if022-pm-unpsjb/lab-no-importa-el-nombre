f942ace30b2aadcfe0fe1fc4c4a848b7e21dfdde
Juan Velasquez <valenelnoobatoyt@gmail.com>
2025-10-10 19:33:56 -0300

    Entrega del laboratorio 4 con API REST

--- lib/router.ex
+++ lib/router.ex
@@ -25,43 +25,48 @@ defmodule Libremarket.Router do
     |> send_resp(200, Jason.encode!(payload))
   end
 
-  put "/compras/:id" do
-
-    id_compra = String.to_integer(id)
-    body = conn.body_params
-
-    if body["medio_de_pago"] do
-      Libremarket.Compras.Server.seleccionar_medio_de_pago(id_compra, body["medio_de_pago"])
-    end
-
-    if body["forma_de_entrega"] do
-      Libremarket.Compras.Server.seleccionar_forma_de_entrega(id_compra, body["forma_de_entrega"])
-    end
-
-    result = Libremarket.Compras.Server.buscar(id_compra)
+  #Esto confirma la compra
+  get "/compras/confirmarCompra/:id" do
+    id = String.to_integer(id)
+    result = Libremarket.Compras.Server.comprar(id)
     {status, compra} = result
     payload = %{status: status, compra: compra}
-
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Jason.encode!(payload))
   end
 
+  #Esto es para actualizar la compra con su medio de Entrega
+  put "/compras/medioEntrega/:id" do
+    id_compra = String.to_integer(id)
+    body = conn.body_params
+    Libremarket.Compras.Server.seleccionar_forma_de_entrega(id_compra, body["forma_de_entrega"])
+    {status, compra} = Libremarket.Compras.Server.buscar(id_compra)
+    payload = %{status: status, compra: compra}
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Jason.encode!(payload))
+  end
+
+  #Esto es para actualizar la compra con su medio de Pago
+  put "/compras/medioPago/:id" do
+    id_compra = String.to_integer(id)
+    body = conn.body_params
+    Libremarket.Compras.Server.seleccionar_medio_de_pago(id_compra, body["medio_de_pago"])
+    {status, compra} = Libremarket.Compras.Server.buscar(id_compra)
+    payload = %{status: status, compra: compra}
+    conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, Jason.encode!(payload))
+  end
+
+  #Esto es para crear una compra nueva y agregarle un producto
   post "/compras" do
     body = conn.body_params
     producto_id = body["producto_id"]
-    forma = body["forma_de_entrega"]
-    medio = body["medio_de_pago"]
-
-    {:ok, id_compra} = Libremarket.Compras.Server.seleccionar_producto(producto_id)
-    Libremarket.Compras.Server.seleccionar_forma_de_entrega(id_compra, forma)
-    Libremarket.Compras.Server.seleccionar_medio_de_pago(id_compra, medio)
-
-    result = Libremarket.Compras.Server.comprar(id_compra)
-
-    {status, compra} = result
+    {status, id_compra} = Libremarket.Compras.Server.seleccionar_producto(producto_id)
+    {status, compra} = Libremarket.Compras.Server.buscar(id_compra)
     payload = %{status: status, compra: compra}
-
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, Jason.encode!(payload))
